7d22a00db85366ce24df00e8e6203db529acb417
yogi hardi <yogi.hardi@gmail.com>
2018-01-28 17:41:16 +0700

    done

--- externalservice/client.go
+++ externalservice/client.go
@@ -1,8 +1,6 @@
 package externalservice
 
-import (
-	"io"
-)
+import "errors"
 
 // Post is the data structure representing the data sent and received from the
 // external service
@@ -17,4 +15,15 @@ type Post struct {
 type Client interface {
 	GET(id int) (*Post, error)
 	POST(id int, post *Post) (*Post, error)
+}
+
+type ClientImpl struct{}
+
+func (c *ClientImpl) GET(id int) (*Post, error) {
+	return nil, errors.New("error")
+}
+
+func (c *ClientImpl) POST(id int, post *Post) (*Post, error) {
+	post.ID = id
+	return post, nil
 }
