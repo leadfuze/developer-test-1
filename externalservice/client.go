0cfd4eceec0c8ec68c23871dbaac45703bb3e86d
blackboat <zygroup1@gmail.com>
2018-01-29 07:36:54 -0200

    first commit

--- externalservice/client.go
+++ externalservice/client.go
@@ -1,7 +1,7 @@
 package externalservice
 
 import (
-	"io"
+	"errors"
 )
 
 // Post is the data structure representing the data sent and received from the
@@ -9,12 +9,33 @@ import (
 type Post struct {
 	ID int `json:"id"` // the primary key
 
-	Title       string `json:"title"`
-	Description string `json:"description,omitempty"`
+	Title       string `json:"title" form:"title"`
+	Description string `json:"description,omitempty" form:"description"`
 }
 
 // Client represents the client interface to the external service
 type Client interface {
 	GET(id int) (*Post, error)
 	POST(id int, post *Post) (*Post, error)
+}
+
+// ClientMock implements Client interfacte
+type ClientMock struct {
+	Posts map[int]*Post
+}
+
+func (c *ClientMock) GET(id int) (*Post, error) {
+	if c.Posts[id] == nil {
+		return nil, errors.New("Post not found")
+	}
+	return c.Posts[id], nil
+}
+
+func (c *ClientMock) POST(id int, post *Post) (*Post, error) {
+	if c.Posts[id] != nil {
+		return nil, errors.New("Post id is already called")
+	}
+	post.ID = id
+	c.Posts[id] = post
+	return post, nil
 }
